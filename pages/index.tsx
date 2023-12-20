ac14011d8943b1d5de8dd394201798fce0898b25
lognguyen <longnt0602@gmail.com>
2023-12-20 13:35:02 -0800

    Added Day of the Week, Fixed some Alignments.

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,12 +13,13 @@ export default function Home() {
   }, 1000);
 
 
-  let month = currentTime.toLocaleString('default', { month: 'long' });
+  let month = currentTime.toLocaleString('default', { month: 'short' });
+  let dayName = currentTime.toLocaleString('default', { weekday: 'short' })
 
   return <div className="container">
   <div className="display-time">
-    <span>{currentTime.getDate()}, {month} {currentTime.getFullYear()} - </span>
     <span suppressHydrationWarning>{currentTime.getHours() + "." + String(currentTime.getMinutes()).padStart(2, '0') + "." + String(currentTime.getSeconds()).padStart(2, '0')}</span>
+    <span>{dayName} {currentTime.getDate()}, {month} {currentTime.getFullYear()}</span>
     </div>
 </div>;
 }
