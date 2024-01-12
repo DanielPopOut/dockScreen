fba42d2eaa6fbf46cfe579468a2e5c74ccf065e7
aidenbull <aidenbull@hotmail.com>
2024-01-11 23:02:05 -0800

    Added test for remove expired events

--- pages/dataProcessing/processEvents.test.ts
+++ pages/dataProcessing/processEvents.test.ts
@@ -1,4 +1,10 @@
+import { TrimOldEvents } from "./processEvents";
 
-test('adds 1 + 2 to equal 3', () => {
-    expect((1+ 2)).toBe(3);
+test('Array of 2 events successfully removes 1 expired event from TrimOldEvents', () => {
+    expect(TrimOldEvents(
+            [{end: {dateTime: "2024-01-10T12:00:00.000Z"}}, {end: {dateTime: "2024-01-10T19:00:00.000Z"}}], 
+            new Date("2024-01-10T17:00:00.000Z"))
+        ).toStrictEqual(
+            [{end: {dateTime: "2024-01-10T19:00:00.000Z"}}]
+        );
   });
