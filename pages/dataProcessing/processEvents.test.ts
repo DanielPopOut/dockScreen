c86e2bc11e66b115b770ed92905ff1d0b111c274
aidenbull <aidenbull@hotmail.com>
2024-01-12 13:00:21 -0800

    Implemented solution for new test. Also added some type safety

--- pages/dataProcessing/processEvents.test.ts
+++ pages/dataProcessing/processEvents.test.ts
@@ -1,31 +1,32 @@
-import { TrimExpiredEvents, SeparateActiveAndUpcomingEvents } from "./processEvents";
+import { TrimExpiredEvents, SeparateStartedAndUpcomingEvents } from "./processEvents";
 
 test('Array of 2 events successfully removes 1 expired event from TrimOldEvents', () => {
     expect(
         TrimExpiredEvents(
             [
-                {end: {dateTime: "2024-01-10T12:00:00.000Z"}}, 
-                {end: {dateTime: "2024-01-10T19:00:00.000Z"}}
+                {end: {dateTime: "2024-01-10T12:00:00.000Z"}, start:{dateTime:""}}, 
+                {end: {dateTime: "2024-01-10T19:00:00.000Z"}, start:{dateTime:""}}
             ], 
             new Date("2024-01-10T17:00:00.000Z")
         )
     ).toStrictEqual(
         [
-            {end: {dateTime: "2024-01-10T19:00:00.000Z"}}
+            {end: {dateTime: "2024-01-10T19:00:00.000Z"}, start:{dateTime:""}}
         ]
     );
 });
 
 test('Array of 1 active event and 1 upcoming event successfully splits into 2 arrays', () => {
-    expect(SeparateActiveAndUpcomingEvents(
+    expect(SeparateStartedAndUpcomingEvents(
         [
-            {start: {dateTime: "2024-01-10T11:00:00.000Z"}, end: {dateTime: "2024-01-10T13:00:00.000Z"}}, 
-            {start: {dateTime: "2024-01-10T14:00:00.000Z"}, end: {dateTime: "2024-01-10T16:00:00.000Z"}}
+            {start: {dateTime: "2024-01-10T11:00:00.000Z"}, end:{dateTime:""}}, 
+            {start: {dateTime: "2024-01-10T14:00:00.000Z"}, end:{dateTime:""}}
         ], 
-        new Date("2024-01-10T17:00:00.000Z")
-    )).toStrictEqual({
-        active: [{start: {dateTime: "2024-01-10T11:00:00.000Z"}, end: {dateTime: "2024-01-10T13:00:00.000Z"}}],
-        upcoming: [{start: {dateTime: "2024-01-10T14:00:00.000Z"}, end: {dateTime: "2024-01-10T16:00:00.000Z"}}]
-    }
+        new Date("2024-01-10T12:00:00.000Z")
+    )).toStrictEqual(
+        {
+            started: [{start: {dateTime: "2024-01-10T11:00:00.000Z"}, end:{dateTime:""}}],
+            upcoming: [{start: {dateTime: "2024-01-10T14:00:00.000Z"}, end:{dateTime:""}}]
+        }
     );
 });
--- pages/dataProcessing/processEvents.ts
+++ pages/dataProcessing/processEvents.ts
@@ -1,6 +1,24 @@
-export function TrimExpiredEvents(events: any, dateTimeToCompare: Date) {
+type OfficeRnDEvent = {
+    start: {dateTime: string};
+    end: {dateTime: string};
+}
+
+export function TrimExpiredEvents(events: Array<OfficeRnDEvent>, dateTimeToCompare: Date) {
     return events.filter((event: any) => {
-        const eventEndTime = new Date(event["end"]["dateTime"])
-        return eventEndTime > dateTimeToCompare
+        const eventEndTime = new Date(event["end"]["dateTime"]);
+        return eventEndTime > dateTimeToCompare;
     })
+}
+
+export function SeparateStartedAndUpcomingEvents(events: Array<OfficeRnDEvent>, dateTimeToCompare: Date) {
+    let out = {started: Array<OfficeRnDEvent>(), upcoming: Array<OfficeRnDEvent>()};
+    for(let i=0; i < Object.keys(events).length; i++) {
+        const item: any = events[i];
+        if (new Date(item["start"]["dateTime"]) < dateTimeToCompare) {
+            out["started"].push(item)
+        } else {
+            out["upcoming"].push(item)
+        }
+    }
+    return out
 }
