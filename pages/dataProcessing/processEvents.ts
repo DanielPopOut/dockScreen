28282a54cb11fa37feccdbe44ab8c95284b6c3e8
aidenbull <aidenbull@hotmail.com>
2024-01-12 10:55:31 -0800

    Renamed TrimOldEvents to TrimExpiredEvents and reformatted the test for it. Also added a new test for separating active events from upcoming events

--- pages/dataProcessing/processEvents.ts
+++ pages/dataProcessing/processEvents.ts
@@ -1,4 +1,4 @@
-export function TrimOldEvents(events: any, dateTimeToCompare: Date) {
+export function TrimExpiredEvents(events: any, dateTimeToCompare: Date) {
     return events.filter((event: any) => {
         const eventEndTime = new Date(event["end"]["dateTime"])
         return eventEndTime > dateTimeToCompare
