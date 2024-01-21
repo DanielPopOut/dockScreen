e8760f3c8c8c54187194da8641e1ebad8d0c4be6
aidenbull <aidenbull@hotmail.com>
2024-01-21 15:19:32 -0800

    Added tests to new methods
    With this we can now safely refactor these methods, and can start extracting the new class. In a larger, more critical codebase, we should more extensively test these methods than I have here but because this is for practice, I'm satisfied with just testing the core behaviours

---
+++ src/services/OfficeRnDService.test.ts
@@ -0,0 +1,45 @@
+import {
+  OfficeRnDService
+} from "./OfficeRnDService"
+
+test('combineMeetingRoomsAndFloors combines two single entry items that match', () => {
+  const service = new OfficeRnDService();
+  expect(
+    service.combineMeetingRoomsAndFloors(
+      {"10": {_id: "10", name: "Test Floor"}},
+      [{_id: "0", name: "Test Room", room: "10"}]
+    )
+  ).toStrictEqual(
+    {"0": {_id: "0", name: "Test Room", room: "10", floor: "Test Floor"}}
+  )
+})
+
+test('combineOfficeRnDData combines a set of single entry data items correctly', () => {
+  const service = new OfficeRnDService();
+  expect(
+    service.combineOfficeRnDData(
+      {"0": {_id: "0", name: "Test Room", room: "10", floor: "Test Floor"}},
+      [{
+        _id: "1", 
+        summary: "", 
+        start: {dateTime: ""}, 
+        end: {dateTime: ""}, 
+        timezone: "", 
+        resourceId: "0", 
+        team: "2"
+      }],
+      {"2": {_id: "2", name: "Test Team"}}
+    )
+  ).toStrictEqual(
+    [{
+      _id: "1",
+      startDateTime: "",
+      endDateTime: "",
+      timezone: "",
+      room: "Test Room",
+      floor: "Test Floor",
+      summary: "",
+      team: "Test Team"
+    }]
+  )
+})
